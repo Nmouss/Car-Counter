b69abe2842fd6379f753d3c6485608c4d71d6d62
Nmouss <97858405+Nmouss@users.noreply.github.com>
2024-08-28 00:51:52 -0400

    car_counter.py

--- yolo/car_counter.py
+++ yolo/car_counter.py
@@ -1,1 +1,7 @@
+-pip3 install ultralytics
 
+import cv2
+from ultralytics import YOLO
+
+model = YOLO("yolov8n.pt")
+results = model.track(source="/Users/nabilmouss/Desktop/ VEHICLE DETECTION/Test Video/Videoo.mp4", show=True, save=False, name='test_result', persist=True, classes=[2, 3, 5, 7]) # this is just taking the video 
