6bbf3ea49cbc8949e9b8796f093554ff15e31217
Nmouss <97858405+Nmouss@users.noreply.github.com>
2024-09-04 22:12:18 -0400

    Update car_counter.py

--- yolo/car_counter.py
+++ yolo/car_counter.py
@@ -1,63 +1,49 @@
--pip3 install ultralytics
-
+import time
 import cv2
 
-from ultralytics import YOLO, solutions
+from ultralytics import YOLO
+from object_counter import ObjectCounter
 
-# from shapely import Polygon
-
-# model = YOLO("yolov8n.pt")
-# results = model.track(source="/Users/nabilmouss/Desktop/ VEHICLE DETECTION/Test Video/Videoo.mp4", show=True, save=False, name='test_result', persist=True, classes=[2, 3, 5, 7]) # this is just taking the video 
-
-
-# Load the pre-trained YOLOv8 model
 model = YOLO("yolov8n.pt")
-
-# Open the video file
-cap = cv2.VideoCapture("/PATH/TO/YOUR/DIRECTORY/VIDEO.mp4")
+cap = cv2.VideoCapture("/Users/nabilmouss/Desktop/ VEHICLE DETECTION/Test Video/videoo.mp4")
 assert cap.isOpened(), "Error reading video file"
-
-# Get video properties: width, height, and frames per second (fps)
 w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
 
-print(w, h)
-# Define points for a line or region of interest in the video frame
-#line_points = [(0, 300), (3840, 300)]  # Line coordinates
-line_points = [(20, 400), (3840, 404), (3840, 360), (20, 360)]
-# Specify classes to count, for example: person (0) and car (2)
-classes_to_count = [2, 3, 5, 7]  # Class IDs for car, truck, motorcycle, bus
+line_points = [(20, 400), (1080, 400)]  # line or region points
+classes_to_count = [2, 3, 5, 7]  # person and car classes for count
 
-# Initialize the video writer to save the output video
-# video_writer = cv2.VideoWriter("object_counting_output.mp4", cv2.VideoWriter_fourcc(*"mp4v"), fps, (w, h))
+# Video writer
+# video_writer = cv2.VideoWriter("object_counting_output.avi", cv2.VideoWriter_fourcc(*"mp4v"), fps, (w, h))
 
-# Initialize the Object Counter with visualization options and other parameters
-counter = solutions.ObjectCounter(
-    view_img=True,  # Display the image during processing
-    reg_pts=line_points,  # Region of interest points
-    names=model.names,  # Class names from the YOLO model
-    draw_tracks=True,  # Draw tracking lines for objects
-    line_thickness=2,  # Thickness of the lines drawn
+# Init Object Counter
+counter = ObjectCounter(
+    view_img=True,
+    reg_pts=line_points,
+    names=model.names,
+    draw_tracks=True,
+    line_thickness=2,
 )
-dictonary = {}
-# Process video frames in a loop
+current_time = time.strftime('%X') # current time
+
+
+dictonary = []
+Counter = 0
 while cap.isOpened():
     success, im0 = cap.read()
     if not success:
         print("Video frame is empty or video processing has been successfully completed.")
         break
-
-    # Perform object tracking on the current frame, filtering by specified classes
     tracks = model.track(im0, persist=True, show=False, classes=classes_to_count)
 
-    # Use the Object Counter to count objects in the frame and get the annotated image
-    im0 = counter.start_counting(im0, tracks)
+    if len(counter.count_ids) != Counter:
+        Counter += 1
+        dictonary.append(counter.is_vehicle)
+    
+    print(dictonary, counter.count_ids)
 
-    # Write the annotated frame to the output video
+    im0 = counter.start_counting(im0, tracks)
     # video_writer.write(im0)
-    print(im0)
-# Release the video capture and writer objects
+print(dictonary, len(dictonary), len(counter.count_ids))
 cap.release()
 # video_writer.release()
-
-# Close all OpenCV windows
 cv2.destroyAllWindows()
