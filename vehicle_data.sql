a91f2b5214dbb4a9f0fbe46b05054e570cd17e18
Nmouss <97858405+Nmouss@users.noreply.github.com>
2024-09-06 16:29:02 -0400

    Update vehicle_data.sql

--- vehicle_data.sql
+++ vehicle_data.sql
@@ -1,4 +1,6 @@
 -- cmd-shift-P "Run query"
 SELECT * FROM vehicle_data;
 
---SELECT * FROM vehicle_data * vehicle_type;
+SELECT vehicle_type, time FROM vehicle_data WHERE vehicle_type="car";
+
+SELECT * FROM vehicle_data WHERE time >= '22:13:50';
