9d07289ee51a413631ce58233f9b23e424ca0ee8
Nmouss <97858405+Nmouss@users.noreply.github.com>
2024-09-06 16:38:53 -0400

    Update vehicle_data.sql

--- vehicle_data.sql
+++ vehicle_data.sql
@@ -4,3 +4,5 @@ SELECT * FROM vehicle_data;
 SELECT vehicle_type, time FROM vehicle_data WHERE vehicle_type="car";
 
 SELECT * FROM vehicle_data WHERE time >= '22:13:50';
+
+SELECT COUNT(*) FROM vehicle_data WHERE time >= '22:13:50';
