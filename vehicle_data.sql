ddf1b1a2c70022f26c4c10518d301086d76fed22
Nmouss <97858405+Nmouss@users.noreply.github.com>
2024-09-07 15:29:23 -0400

    Update vehicle_data.sql

--- vehicle_data.sql
+++ vehicle_data.sql
@@ -3,6 +3,7 @@ SELECT * FROM vehicle_data;
 
 SELECT vehicle_type, time FROM vehicle_data WHERE vehicle_type="car";
 
+-- Select all the columns where time is greater than or equal to '22:13:50'
 SELECT * FROM vehicle_data WHERE time >= '22:13:50';
 
 SELECT COUNT(*) FROM vehicle_data WHERE time >= '22:13:50';
